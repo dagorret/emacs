bfa9cbf6a27f1d50dd8a0511e7a4dc99a78ce7ac
Carlos Dagorret <dagorret@gmail.com>
2025-11-23 16:05:50 -0300

    Agregar módulo core-base con ajustes básicos

---
+++ lisp/core-base.el
@@ -0,0 +1,38 @@
+;;; core-base.el --- Ajustes básicos de Emacs -*- lexical-binding: t; -*-
+
+;; Sin pantalla de inicio ni mensajes molestos
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+
+;; Silenciar campana
+(setq ring-bell-function 'ignore)
+
+;; UI más limpia
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Fuente un poco más grande (110 = 11pt aprox)
+(set-face-attribute 'default nil :height 110)
+
+;; Números de línea relativos
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
+
+;; Mostrar emparejamiento de paréntesis
+(show-paren-mode 1)
+
+;; Backups en carpeta separada, sin autosave por todos lados
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name "backups" user-emacs-directory)))
+      auto-save-default nil)
+
+;; Usar espacios en vez de tabs
+(setq-default indent-tabs-mode nil)
+
+;; Autocerrar paréntesis, comillas, etc.
+(electric-pair-mode 1)
+
+(provide 'core-base)
+;;; core-base.el ends here
+
